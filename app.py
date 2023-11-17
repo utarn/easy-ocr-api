3d88d139e2a711c7dd423a75c1e16716581c94c3
Utharn Buranasaksee <utarn@yahoo.com>
2023-11-17 13:23:16 +0700

    Add text ocr endpoint

--- app.py
+++ app.py
@@ -1,15 +1,77 @@
-from flask import Flask, request, jsonify, send_file
-import easyocr
 import base64
-import numpy as np
-import cv2
-from PIL import Image
-import dlib
 import io
+
+import cv2
+import dlib
+import easyocr
+import numpy as np
+from flask import Flask, jsonify, request, send_file
+from PIL import Image
+from pythainlp.spell import correct_sent
+from pythainlp.tokenize import word_tokenize
+from textblob import TextBlob
 
 # Load Dlib's pre-trained facial landmark predictor
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
+
+def is_english(word):
+    return all('\u0000' <= char <= '\u007F' for char in word)
+
+def is_thai(word):
+    return all('\u0E00' <= char <= '\u0E7F' for char in word)
+
+def eng_correction(text):
+    new_text = TextBlob(text)
+    correct_word = new_text.correct()
+    print (correct_word)
+    return str(correct_word)
+
+def thai_correction(text):
+    corrected_word=correct_sent(word_tokenize(text, None, 'deepcut'), engine='wanchanberta_thai_granmarly')
+    append_word = ''.join(corrected_word)
+    return append_word
+
+def process_text(text):
+    words = text.split()
+    current_language = None
+    language_buffer = []
+    output = []
+
+    for word in words:
+        if is_english(word):
+            if current_language != 'english':
+                if language_buffer:
+                    corrected_text = thai_correction(' '.join(language_buffer))
+                    print(f"Thai Corrected1: {corrected_text}")
+                    output.append(corrected_text)
+                language_buffer = []
+                current_language = 'english'
+            language_buffer.append(word)
+        elif is_thai(word):
+            if current_language != 'thai':
+                if language_buffer:
+                    corrected_text = eng_correction(' '.join(language_buffer))
+                    print(f"English Corrected1: {corrected_text}")
+                    output.append(corrected_text)
+                language_buffer = []
+                current_language = 'thai'
+            print(word)
+            output.append(thai_correction(word))
+            #lsdfanguage_buffer.append(word)
+
+    # Process remaining words in buffer
+    if language_buffer:
+        if current_language == 'english':
+            corrected_text = eng_correction(' '.join(language_buffer))
+            output.append(corrected_text)
+            print(f"English Corrected2: {corrected_text}")
+        else:
+            corrected_text = thai_correction(' '.join(language_buffer))
+            output.append(corrected_text)
+            print(f"Thai Corrected2: {corrected_text}")
+
+    return " ".join(output)
 
 def detect_eyes(img_np):
     gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
@@ -43,6 +105,37 @@ def detect_eyes(img_np):
 app = Flask(__name__)
 reader = easyocr.Reader(['en','th'], download_enabled=False)  # Assuming English text
 
+@app.route('/text', methods=['POST'])
+def process_text_endpoint():
+    try:
+        # Decode the image from base64
+        data = request.json
+        image_b64 = data['image']
+        decoded_image = base64.b64decode(image_b64)
+
+        # Convert the decoded bytes to a numpy array
+        nparr = np.frombuffer(decoded_image, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
+        # Check if the image has an alpha channel (RGBA) and convert to RGB
+        if img_np.shape[-1] == 4:  # Image has 4 channels
+            img_pil = Image.fromarray(img_np)
+            img_pil = img_pil.convert('RGB')
+            img_np = np.array(img_pil)
+
+        # OCR processing
+        results = reader.readtext(img_np)
+
+        processed_texts = []
+        for (bbox, text, prob) in results:
+            processed_text = process_text(text)
+            processed_texts.append(processed_text)
+
+        final_result = ' '.join(processed_texts)
+        return jsonify({'text': final_result})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
 @app.route('/ocr', methods=['POST'])
 def ocr():
     try:
